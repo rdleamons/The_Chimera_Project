f267906e2cc4cb3cc01e087f3038000bf7e7d484
Unknown <rdleamons@gmail.com>
2020-02-25 10:24:48 -0600

    2/24 Class Push
    Adds tile maps, Cinemachine, attempts at player health/enemy attack.

---
+++ The Chimera Project/Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth = 100;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    private void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        if(currentHealth <= 0)
+        {
+            die();
+        }
+    }
+
+    void die()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    //public void Damage(int dmg)
+    //{
+        //currentHealth -= dmg;
+    //}
+}
