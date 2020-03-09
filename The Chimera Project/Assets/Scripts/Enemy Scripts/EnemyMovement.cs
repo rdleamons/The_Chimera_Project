6b5b287f3405324f77849400cd644c456abfa90c
Unknown <rdleamons@gmail.com>
2020-03-09 18:56:59 -0500

    3/9 Class Push
    Enemies wander, but WAAAY too far outside of the scene bounds. They also can't go up.

--- The Chimera Project/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
+++ The Chimera Project/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public bool inCombat;
+    public float wanderTime;
+    public float moveSpeed = 1f;
 
     public Rigidbody2D rb;
 
+    GameObject thePlayer;
+    public Player player;
+
     Vector2 movement;
 
+    private void Start()
+    {
+        thePlayer = GameObject.Find("Player");
+        player = thePlayer.GetComponent<Player>();
+    }
     private void Update()
     {
-        movement.x = Random.Range(-5, 5);
-        movement.y = Random.Range(-5, 5);
+        if(player.enemyDead == false)
+        {
+            if(wanderTime > 0)
+            {
+                transform.Translate(Vector3.forward * moveSpeed);
+                wanderTime -= Time.deltaTime;
+            }
+            else
+            {
+                wanderTime = Random.Range(-1f, 1f);
+                wander();
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void wander()
+    {
+        transform.eulerAngles = new Vector3(0, Random.Range(0, 360), 0);
     }
 }
