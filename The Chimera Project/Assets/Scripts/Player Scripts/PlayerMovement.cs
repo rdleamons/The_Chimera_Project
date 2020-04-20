4be2463a327417909f61667230ca8f0b4a2a3a4f
Unknown <rdleamons@gmail.com>
2020-04-20 16:34:46 -0500

    Adds health vial art and basic sprint function
    Still need bar for sprint, but it works on a basic level using shift.

--- The Chimera Project/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ The Chimera Project/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public int stamina = 2;
+    private int maxStamina = 2;
 
     public Rigidbody2D rb;
 
@@ -13,6 +15,24 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) & stamina > 0)
+        {
+            CancelInvoke();
+            moveSpeed = 10f;
+            InvokeRepeating("loseStamina", 0, 0.5f);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) || stamina == 0)
+        {
+            CancelInvoke();
+            moveSpeed = 5f;
+            InvokeRepeating("gainStamina", 0, 0.1f);
+        }
+
+        if (stamina >= maxStamina)
+        {
+            stamina = maxStamina;
+        }
     }
 
     private void FixedUpdate()
@@ -20,4 +40,13 @@ public class PlayerMovement : MonoBehaviour
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
+    private void loseStamina()
+    {
+        stamina--;
+    }
+
+    private void gainStamina()
+    {
+        stamina++;
+    }
 }
